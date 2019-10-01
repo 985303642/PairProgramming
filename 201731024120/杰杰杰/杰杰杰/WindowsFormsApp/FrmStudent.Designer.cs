f96a5fbc3842db6cb9aa2d62053e1ca082364c09
985303642 <985303642@qq.com>
2019-10-01 09:04:39 +0800

    第一次结对作业提交

---
+++ 201731024120/杰杰杰/杰杰杰/WindowsFormsApp/FrmStudent.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApp
+{
+    partial class FrmStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgvStuList = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLoadStu = new System.Windows.Forms.Button();
+            this.progressName = new System.Windows.Forms.ProgressBar();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.timerCallName = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStuList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 49);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "学生列表";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Info;
+            this.panel3.Controls.Add(this.btnLoadStu);
+            this.panel3.Controls.Add(this.btnStop);
+            this.panel3.Controls.Add(this.btnStart);
+            this.panel3.Location = new System.Drawing.Point(0, 278);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(656, 121);
+            this.panel3.TabIndex = 2;
+            // 
+            // dgvStuList
+            // 
+            this.dgvStuList.AllowUserToAddRows = false;
+            this.dgvStuList.AllowUserToDeleteRows = false;
+            this.dgvStuList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStuList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dgvStuList.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvStuList.Location = new System.Drawing.Point(0, 49);
+            this.dgvStuList.Name = "dgvStuList";
+            this.dgvStuList.ReadOnly = true;
+            this.dgvStuList.RowTemplate.Height = 23;
+            this.dgvStuList.Size = new System.Drawing.Size(800, 169);
+            this.dgvStuList.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "学号";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "姓名";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(79, 43);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(67, 31);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始点名";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(189, 44);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(77, 30);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "停止点名";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnLoadStu
+            // 
+            this.btnLoadStu.Location = new System.Drawing.Point(416, 43);
+            this.btnLoadStu.Name = "btnLoadStu";
+            this.btnLoadStu.Size = new System.Drawing.Size(78, 30);
+            this.btnLoadStu.TabIndex = 2;
+            this.btnLoadStu.Text = "加载学生";
+            this.btnLoadStu.UseVisualStyleBackColor = true;
+            this.btnLoadStu.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressName
+            // 
+            this.progressName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressName.Location = new System.Drawing.Point(0, 218);
+            this.progressName.Name = "progressName";
+            this.progressName.Size = new System.Drawing.Size(800, 63);
+            this.progressName.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 395);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(800, 55);
+            this.panel4.TabIndex = 4;
+            // 
+            // FrmStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.progressName);
+            this.Controls.Add(this.dgvStuList);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmStudent";
+            this.Text = "FrmStudent";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStuList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dgvStuList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLoadStu;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.ProgressBar progressName;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Timer timerCallName;
+    }
+}
